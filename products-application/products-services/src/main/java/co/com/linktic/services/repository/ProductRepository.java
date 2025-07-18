db1c0de8298760898d57662166c2df35b368a93d
Michael Vega <micha3lvega@gmail.com>
2025-07-17 22:04:23 -0500

    feat: crear método para obtener los productos paginados

--- products-application/products-services/src/main/java/co/com/linktic/services/repository/ProductRepository.java
+++ products-application/products-services/src/main/java/co/com/linktic/services/repository/ProductRepository.java
@@ -1,9 +1,14 @@
 package co.com.linktic.services.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import co.com.linktic.services.entity.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+	@Override
+	Page<Product> findAll(Pageable pageable);
 
 }
